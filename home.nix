6c841c8670846b2882a5a04a2bbab705093e1091
George Wyatt <georgewyatt@fastmail.com>
2021-09-06 23:05:46 +0800

    Fix indentation

--- home.nix
+++ home.nix
@@ -1,27 +1,27 @@
 { config, pkgs, ... }:
-  {
-    home = {
-      username = "george";
-      homeDirectory = "/Users/george";
-      stateVersion = "21.11"; # DO NOT CHANGE
-    };
+{
+  home = {
+    username = "george";
+    homeDirectory = "/Users/george";
+    stateVersion = "21.11"; # DO NOT CHANGE
+  };
 
-    imports = [
-      ./bat.nix
-      ./darwin.nix
-      ./direnv.nix
-      ./fzf.nix
-      ./home-manager.nix
-      ./htop.nix
-      ./lazygit.nix
-      ./neovim.nix
-      ./packages.nix
-      ./session.nix
-      ./zsh.nix
-    ];
+  imports = [
+    ./bat.nix
+    ./darwin.nix
+    ./direnv.nix
+    ./fzf.nix
+    ./home-manager.nix
+    ./htop.nix
+    ./lazygit.nix
+    ./neovim.nix
+    ./packages.nix
+    ./session.nix
+    ./zsh.nix
+  ];
 
-    # TODO: programs.gh
-    # TODO: programs.git
-    # TODO: programs.gpg
-    # TODO: programs.ssh
-  }
+  # TODO: programs.gh
+  # TODO: programs.git
+  # TODO: programs.gpg
+  # TODO: programs.ssh
+}
