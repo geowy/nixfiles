a61d14f478df09407132a567179eaccf5c30c423
George Wyatt <georgewyatt@fastmail.com>
2021-09-08 11:16:35 +0800

    Use JSON for glow config

--- glow.nix
+++ glow.nix
@@ -1,16 +1,10 @@
 { pkgs, ... }:
 {
   home.packages = [ pkgs.glow ];
-  home.file."Library/Preferences/glow/glow.yml".text = ''
-    # style name or JSON path (default "auto")
-    style: "light"
-    # show local files only; no network (TUI-mode only)
-    local: true
-    # mouse support (TUI-mode only)
-    mouse: true
-    # use pager to display markdown
-    pager: false
-    # word-wrap at width
-    width: 80
-  '';
+
+  home.file."Library/Preferences/glow/glow.yml".text = builtins.toJSON {
+    style = "light";
+    local = true;
+    mouse = true;
+  };
 }
