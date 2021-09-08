3892aec869b634b9d943fbbac8cfc514dc4a201d
George Wyatt <georgewyatt@fastmail.com>
2021-09-08 11:01:44 +0800

    Add glow config

--- home.nix
+++ home.nix
@@ -6,6 +6,15 @@
     stateVersion = "21.11"; # DO NOT CHANGE
   };
 
+  home.sessionPath = [
+    "${config.home.homeDirectory}/bin"
+  ];
+
+  home.sessionVariables = {
+    CLICOLOR = 1;
+    EDITOR = "nvim";
+  };
+
   home.packages = with pkgs; [
     aws-vault
     awscli
@@ -13,7 +22,6 @@
     deno
     ffmpeg
     fswatch
-    glow
     go
     httpie
     jq
@@ -26,21 +34,13 @@
     youtube-dl
   ];
 
-  home.sessionPath = [
-    "${config.home.homeDirectory}/bin"
-  ];
-
-  home.sessionVariables = {
-    CLICOLOR = 1;
-    EDITOR = "nvim";
-  };
-
   imports = [
     ./bat.nix
     ./darwin.nix
     ./direnv.nix
     ./fzf.nix
     ./git.nix
+    ./glow.nix
     ./gotop.nix
     ./gpg.nix
     ./home-manager.nix
